092416896873a7266c75484a37f42d964bb7f1c7
Dion van der Wateren <39389180+DionWateren@users.noreply.github.com>
2020-04-23 13:32:36 +0200

    added docking parents

--- gamemode/vgui/menu_main.lua
+++ gamemode/vgui/menu_main.lua
@@ -26,12 +26,68 @@ local PANEL = {
 		    gui.EnableScreenClicker( false )
         end
 
+        local label = vgui.Create( "DLabel", self)
+        label:SetFont( "MyFont" )
+        label:SetText( "Derma and Panel Test Menu" )
+        label:SetPos( 4, 4 )
+        label:SizeToContents()
+        --label:SetBright( true )
+        --label:SetDark( true )
+
+        local mainpanel = vgui.Create( "DPanel", self )
+        mainpanel:SetPos( 3, 35 )
+        mainpanel:SetSize( x - 6, y - 35 - 3 )
+        mainpanel.Paint = function( self, w, h )
+            draw.RoundedBox( 0, 0, 0, w, h, Color( 100, 100, 100, 100 ) )
+        end
+
+        local colsheet = vgui.Create( "DColumnSheet", mainpanel )
+        colsheet:Dock( FILL )
+        
+        local sheet1 = vgui.Create( "DPanel", colsheet )
+        sheet1:Dock( FILL )
+        sheet1.Paint = function( self, w, h )
+            draw.RoundedBox( 0, 0, 0, w, h, Color( 10, 10, 10, 100 ) )
+        end
+        colsheet:AddSheet( "Color Mixer", sheet1, "icon16/accept.png" )
+
+        local colormixer = vgui.Create( "DColorMixer", sheet1 )
+        --colormixer:Center()
+        colormixer:Dock( TOP )
+        colormixer:DockMargin( 15, 10, 15, 10 )
+        colormixer:DockPadding( 290, 10, 290, 10 )
+        colormixer:SetPalette( true )
+        colormixer:SetAlphaBar( true )
+        colormixer:SetWangs( true )
+        colormixer:SetColor( Color( 10, 90, 180 ) )
+
+        local mixbutton = vgui.Create( "DButton", sheet1 )
+        mixbutton:SetText( "Enable" )
+        mixbutton:Dock( TOP )
+        mixbutton:DockMargin( 385, 10, 385, 10 )
+        --mixbutton:SetSize( 50, 30 )
+        --mixbutton:SetPos( x/2 - 25, y/2 - 15 + 140 )
+        mixbutton.DoClick = function()
+            function self:Paint( w, h )
+                draw.RoundedBox( 0, 0, 0, w, h, colormixer:GetColor() )
+                surface.SetDrawColor( 255, 255, 255)
+                surface.DrawOutlinedRect( 2, 2, w-4, h-4 )
+            end
+        end
+
+        local sheet2 = vgui.Create( "DPanel", colsheet )
+        sheet2:Dock( FILL )
+        sheet2.Paint = function( self, w, h )
+            draw.RoundedBox( 0, 0, 0, w, h, Color( 10, 10, 10, 100 ) )
+        end
+        colsheet:AddSheet( "????", sheet2, "icon16/anchor.png" )
+
     end,
 
     Paint = function( self, w, h )
 
-        draw.RoundedBox( 0, 0, 0, w, h, Color( 10, 10, 10, 150 ))
-        surface.SetDrawColor( 255, 255, 255)
+        draw.RoundedBox( 0, 0, 0, w, h, Color( 10, 10, 10, 150 ) )
+        surface.SetDrawColor( 255, 255, 255 )
         surface.DrawOutlinedRect( 2, 2, w-4, h-4 )
 
     end
